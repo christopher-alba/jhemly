77d0e00d25d21f216d53c89f022be4ff40fe53da
albasychris <albasychris@gmail.com>
2020-05-04 09:23:25 +1200

    complete getProduct route and function

--- server/db/productsdb.js
+++ server/db/productsdb.js
@@ -1,9 +1,17 @@
 const connection = require('./index')
 
 module.exports = {
-  getAll
+  getAll,
+  getProduct
 }
 
 function getAll (db = connection) {
   return db('products').select()
+}
+
+function getProduct (id, db = connection) {
+  return db('products')
+    .where('product_id', id)
+    .select()
+    .first()
 }
