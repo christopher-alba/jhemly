aced2bdda85a201ba31ed35f36e50e36d2b6c4df
albasychris <albasychris@gmail.com>
2020-05-16 16:27:59 +1200

    add error handling inside usersdb

--- server/db/usersdb.js
+++ server/db/usersdb.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const connection = require('./index')
 
 module.exports = {
@@ -9,7 +10,9 @@ module.exports = {
 }
 
 function getAll (db = connection) {
-  return db('users').select()
+  return db('users')
+    .select()
+    .catch(err => errorHandler(err, 'getAll'))
 }
 
 function getUser (id, db = connection) {
@@ -17,21 +20,28 @@ function getUser (id, db = connection) {
     .where('User_id', id)
     .select()
     .first()
+    .catch(err => errorHandler(err, 'getUser'))
 }
 
 function addUser (User, db = connection) {
   return db('users')
     .insert({ ...User })
+    .catch(err => errorHandler(err, 'addUser'))
 }
 
 function updateUser (updates, id, db = connection) {
   return db('users')
     .where('User_id', id)
     .update({ ...updates })
+    .catch(err => errorHandler(err, 'updateUser'))
 }
 
 function deleteUser (id, db = connection) {
   return db('users')
     .where('User_id', id)
     .del()
+    .catch(err => errorHandler(err, 'deleteUser'))
+}
+function errorHandler (err, location) {
+  console.log(`You have an error in ${location} function inside usersdb. \n ${err.message}`)
 }
