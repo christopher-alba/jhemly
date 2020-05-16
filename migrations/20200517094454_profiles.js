3383728cb46f01b52d4431d55cd573622d595e55
albasychris <albasychris@gmail.com>
2020-05-17 09:50:48 +1200

    make user_id for profiles unique FK

--- migrations/20200517094454_profiles.js
+++ migrations/20200517094454_profiles.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('profiles', (table) => {
     table.increments('id').primary()
-    table.integer('user_id').references('users.user_id')
+    table.integer('user_id').references('users.user_id').unique()
     table.string('user_name').unique()
     table.string('profile_image')
     table.string('mailing_address')
