8898686162dc38aa780bb44c87ad4f9da8d2779b
albasychris <albasychris@gmail.com>
2020-05-16 16:49:20 +1200

    compelte add user test

---
+++ server/db/tests/usersdb.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+const knex = require('knex')
+const config = require('../../../knexfile').test
+const testDb = knex(config)
+
+const db = require('../usersdb')
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+test('addUser adds a new user', () => {
+  const user = {
+    user_name: 'chris',
+    user_picture: 'a picture1',
+    email: 'randomemail1@email.com',
+    mailing_address: '1randomstreet' }
+  return db.addUser(user, testDb)
+    .then(() => {
+      return db.getAll(testDb)
+        .then(users => {
+          expect(users).toHaveLength(7)
+        })
+    })
+})
