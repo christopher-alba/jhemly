eca629d8a720d534b28de056eb29361613d9c811
albasychris <albasychris@gmail.com>
2020-05-17 09:49:28 +1200

    add profile migration

---
+++ migrations/20200517094454_profiles.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('profiles', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').references('users.user_id')
+    table.string('user_name').unique()
+    table.string('profile_image')
+    table.string('mailing_address')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('profiles')
+}
