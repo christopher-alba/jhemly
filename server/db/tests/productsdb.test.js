e61afa37f1b3385a4cc82f91c79a42e47ade2d23
albasychris <albasychris@gmail.com>
2020-05-16 14:31:41 +1200

    write test for productsdb add product

---
+++ server/db/tests/productsdb.test.js
@@ -0,0 +1,29 @@
+const knex = require('knex')
+const config = require('./knexfile').test
+const testDb = knex(config)
+
+const db = require('../productsdb')
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+test('addProduct adds a new product', () => {
+  const product = {
+    product_name: 'productname7',
+    product_picture: 'productpicture7',
+    product_price: 0.99,
+    favourites_count: 7,
+    size_id: 7 }
+  return db.addProduct(product, testDb)
+    .then(() => {
+      testDb('products').select()
+        .then(products => {
+          expect(products).toHaveLength(4)
+        })
+    })
+})
