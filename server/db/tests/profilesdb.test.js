d19bcf5c70f08b6a350a9a312ac6461ed741a933
albasychris <albasychris@gmail.com>
2020-05-19 15:39:03 +1200

    add addprofile test for return value

--- server/db/tests/profilesdb.test.js
+++ server/db/tests/profilesdb.test.js
@@ -39,6 +39,19 @@ test('addProfile adds a new profile', () => {
     })
 })
 
+test('addProfile returns the new array of profiles', () => {
+  const profile = {
+    user_id: 7,
+    user_name: 'Seven',
+    profile_image: 'random image 7',
+    mailing_address: 'street7'
+  }
+  return db.addProfile(profile, testDb)
+    .then(profiles => {
+      expect(profiles).toHaveLength(7)
+    })
+})
+
 test('updateProfile updates the profile', () => {
   const updates = {
     user_name: 'moooo'
