f95dec02e3756a265619fed62fa391e94ce51f5e
albasychris <albasychris@gmail.com>
2020-05-19 16:51:00 +1200

    set up mocks for usersdb in testing users routes

--- server/routes/tests/users.test.js
+++ server/routes/tests/users.test.js
@@ -11,11 +11,40 @@ jest.mock('../../middleware/index', () => {
     }
   }
 })
+jest.mock('../../db/usersdb', () => {
+  return {
+    getAll: () => {
+      return Promise.resolve()
+    },
 
-test('Test if getAll route is working (as admin)', () => {
+    getUser: () => {
+      return Promise.resolve()
+    },
 
+    addUser: () => {
+      return Promise.resolve()
+    },
+
+    updateUser: () => {
+      return Promise.resolve()
+    },
+
+    deleteUser: () => {
+      return Promise.resolve()
+    }
+  }
+})
+test('Test if getAll route is working', () => {
   return request(server)
     .get('/api/v1/users')
+    .then(res => {
+      expect(res.status).toBe(200)
+    })
+})
+
+test('Test if getUser route is working', () => {
+  return request(server)
+    .get('/api/v1/users/1')
     .then(res => {
       expect(res.status).toBe(200)
     })
