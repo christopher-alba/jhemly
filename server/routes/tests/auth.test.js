9babcc39d275ced134b321b6663a603dc71fbf5c
albasychris <albasychris@gmail.com>
2020-05-23 16:27:12 +1200

    add test for login route correct

---
+++ server/routes/tests/auth.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const server = require('../../server')
+
+jest.mock('../../db/auth', () => {
+  return {
+    authenticate: () => {
+      return {
+        name: 'chris',
+        admin: true
+      }
+    },
+    newUser: () => {
+      return {
+        user_id: 1,
+        email: 'randomemail1@email.com',
+        user_name: 'name'
+      }
+    }
+  }
+})
+
+test('test if authenticate route works', () => {
+  return request(server)
+    .post('/api/v1/auth/login')
+    .then(res => expect(res.status).toBe(200))
+})
