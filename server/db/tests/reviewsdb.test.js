9714a172f89b33d008e83853868b18e5920247e3
albasychris <albasychris@gmail.com>
2020-05-25 10:37:25 +1200

    test if getReviews and getAll works

---
+++ server/db/tests/reviewsdb.test.js
@@ -0,0 +1,22 @@
+const knex = require('knex')
+const config = require('../../../knexfile').test
+const testDb = knex(config)
+
+const db = require('../reviewsdb')
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+test('test if getAll works', () => {
+  return db.getAll(testDb)
+    .then(reviews => expect(reviews).toHaveLength(5))
+})
+
+test('test if getReviews works', () => {
+  return db.getReviews(1, testDb)
+    .then(reviews => expect(reviews).toHaveLength(5))
+})
