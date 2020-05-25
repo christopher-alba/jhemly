bb929ed1926eec02669d7320a67e357b86e3255d
albasychris <albasychris@gmail.com>
2020-05-26 10:21:27 +1200

    test addOrder works

--- server/db/tests/ordersdb.test.js
+++ server/db/tests/ordersdb.test.js
@@ -16,3 +16,13 @@ test('test if getOrder works', () => {
   return db.getOrder(1, testDb)
     .then(order => expect(order.order_id).toBe(1))
 })
+
+test('test if getOrders works', () => {
+  return db.getOrders(0, testDb)
+    .then(orders => expect(orders).toHaveLength(2))
+})
+
+test('test if addOrder works', () => {
+  return db.addOrder({}, testDb)
+    .then(orders => expect(orders).toHaveLength(6))
+})
