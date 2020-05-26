e7d3e7f65c7a0cc00f8c22f5f669605237bcfeae
albasychris <albasychris@gmail.com>
2020-05-27 11:57:47 +1200

    test userscartdb functions

---
+++ server/db/tests/userscartdb.test.js
@@ -0,0 +1,33 @@
+const knex = require('knex')
+const config = require('../../../knexfile').test
+const testDb = knex(config)
+
+const db = require('../userscartdb')
+
+beforeAll(() => testDb.migrate.latest())
+beforeEach(() => testDb.seed.run())
+
+test('test that getAll works', () => {
+  return db.getAll(testDb)
+    .then(items => expect(items).toHaveLength(3))
+})
+
+test('test that getUserCart works', () => {
+  return db.getUserCart(1, testDb)
+    .then(items => expect(items).toHaveLength(1))
+})
+
+test('test that addCartItem works', () => {
+  return db.addCartItem({ user_id: 1, product_id: 2 }, testDb)
+    .then(items => expect(items).toHaveLength(2))
+})
+
+test('test that deleteCartItem works', () => {
+  return db.deleteCartItem(1, 1, testDb)
+    .then(() => {
+      return testDb('users_cart')
+        .where('user_id', 1)
+        .select()
+    })
+    .then(items => expect(items).toHaveLength(0))
+})
---
+++ server/db/userscartdb.js
@@ -0,0 +1,49 @@
+const connection = require('./index')
+
+module.exports = {
+  getAll,
+  getUserCart,
+  deleteCartItem,
+  clearUserCart,
+  addCartItem
+}
+
+function getAll (db = connection) {
+  return db('users_cart')
+    .select()
+    .catch(err => errorHandler('getAll', err))
+}
+
+function getUserCart (id, db = connection) {
+  return db('users_cart')
+    .where('user_id', id)
+    .select()
+    .catch(err => errorHandler('getUserCart', err))
+}
+
+function addCartItem (item, db = connection) {
+  return db('users_cart')
+    .insert({ ...item })
+    .then(() => {
+      return db('users_cart')
+        .where('user_id', item.user_id)
+        .select()
+    })
+    .catch(err => errorHandler('addCartItem', err))
+}
+function deleteCartItem (userId, productId, db = connection) {
+  return db('users_cart')
+    .where({ user_id: userId, product_id: productId })
+    .del()
+}
+
+function clearUserCart (id, db = connection) {
+  return db('users_cart')
+    .where('user_id', id)
+    .catch(err => errorHandler('getUserCart', err))
+}
+
+function errorHandler (location, error) {
+  // eslint-disable-next-line no-console
+  console.log(`There is an error in ${location} in userscartdb.js \n ${error.message}`)
+}
