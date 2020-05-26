d9bce935b12fd6533af2056647dce6fdcc7c2623
albasychris <albasychris@gmail.com>
2020-05-26 15:30:39 +1200

    write ordersproductsdb functions

--- server/db/ordersproductsdb.js
+++ server/db/ordersproductsdb.js
@@ -1,13 +1,37 @@
 const connection = require('./index')
 
 module.exports = {
-  getAll
+  getAll,
+  getByOrderId,
+  deleteOrderItem,
+  addOrderItem
 }
 
 function getAll (db = connection) {
   return db('orders_products')
     .select()
     .catch(err => errorHandler('getAll', err))
+}
+
+function getByOrderId (id, db = connection) {
+  return db('orders_products')
+    .where('order_id', id)
+    .select()
+    .catch(err => errorHandler('getByOrderId', err))
+}
+
+function addOrderItem (item, db = connection) {
+  return db('orders_products')
+    .insert({ ...item })
+    .then(() => db('orders_products').where('order_id', item.order_id).select())
+    .catch(err => errorHandler('addOrderItem', err))
+}
+
+function deleteOrderItem (orderId, productId, db = connection) {
+  return db('orders_products')
+    .where({ order_id: orderId, product_id: productId })
+    .del()
+    .catch(err => errorHandler('deleteOrder', err))
 }
 
 function errorHandler (location, error) {
