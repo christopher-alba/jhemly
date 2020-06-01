a3698130db1ee85dd5693c5f6b31a0d701909b0b
albasychris <albasychris@gmail.com>
2020-06-01 15:05:39 +1200

    add productscategories db functions and routes

---
+++ server/db/productscategoriesdb.js
@@ -0,0 +1,44 @@
+const connection = require('./index')
+
+module.exports = {
+  getAll,
+  getProductCats,
+  addProductCat,
+  deleteProductCat
+}
+function getAll (db = connection) {
+  return db('products_categories')
+    .select()
+    .catch(err => errorHandler('getAll', err))
+}
+
+function getProductCats (id, db = connection) {
+  return db('products_categories')
+    .where('product_id', id)
+    .select()
+    .catch(err => errorHandler('getProductCats', err))
+}
+
+function addProductCat (product, db = connection) {
+  return db('product_categories')
+    .insert({ ...product })
+    .then(() => {
+      return db('products_categories').select()
+    })
+    .catch(err => errorHandler('addProductCat', err))
+}
+
+function deleteProductCat (productId, categoryId, db = connection) {
+  return db('products_categories')
+    .where({
+      product_id: productId,
+      category_id: categoryId
+    })
+    .del()
+    .catch(err => errorHandler('deleteProductCat', err))
+}
+
+function errorHandler (location, error) {
+  // eslint-disable-next-line no-console
+  console.log(`There is an error in ${location} inside productscategoriesdb.js. \n ${error.message}`)
+}
---
+++ server/routes/productscategories.js
@@ -0,0 +1,38 @@
+const db = require('../db/productscategoriesdb')
+const express = require('express')
+const camelcaseKeys = require('camelcase-keys')
+const { isAdmin } = require('../middleware/index')
+const router = express.Router()
+module.exports = router
+
+// GET /api/v1/productscategories
+router.get('/', (req, res) => {
+  db.getAll()
+    .then(camelcaseKeys)
+    .then(items => res.status(200).json(items))
+    .catch(err => res.status(500).send(err.message))
+})
+
+// GET /api/v1/productscategories/:id
+router.get('/:id', (req, res) => {
+  db.getProductCats(req.params.id)
+    .then(camelcaseKeys)
+    .then(items => res.status(200).json(items))
+    .catch(err => res.status(500).send(err.message))
+})
+
+// POST /api/v1/productscategories
+router.post('/', isAdmin, (req, res) => {
+  db.addProductCat(req.body)
+    .then(camelcaseKeys)
+    .then(items => res.status(200).json(items))
+    .catch(err => res.status(500).send(err.message))
+})
+
+// DELETE /api/v1/productscategories
+router.delete('/', isAdmin, (req, res) => {
+  db.deleteProductCat(req.body.productId, req.body.categoryId)
+    .then(camelcaseKeys)
+    .then(id => res.status(200).json(id))
+    .catch(err => res.status(500).send(err.message))
+})
