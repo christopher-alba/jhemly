9cf8dc035d1801571f45006bbd64698f926c8b5d
albasychris <albasychris@gmail.com>
2020-06-09 13:58:20 +1200

    write favouriteproducts db funcitons

---
+++ server/db/favouriteproductsdb.js
@@ -0,0 +1,32 @@
+const connection = require('index')
+
+module.exports = {
+  getUserFavourites,
+  addUserFavourite,
+  deleteUserFavourite
+}
+
+function getUserFavourites (id, db = connection) {
+  db('favourite_products')
+    .where('user_id', id)
+    .select()
+    .catch(err => errorHandler('getUserFavourites', err))
+}
+
+function addUserFavourite (item, db = connection) {
+  db('favourite_products')
+    .insert({ ...item })
+    .catch(err => errorHandler('addUserFavourite', err))
+}
+
+function deleteUserFavourite (item, db = connection) {
+  db('favourite_products')
+    .where({ ...item })
+    .del()
+    .catch(err => errorHandler('addUserFavourite', err))
+}
+
+function errorHandler (location, error) {
+  // eslint-disable-next-line no-console
+  console.log(`There is an error in ${location} in favouriteproductsdb.js. \n ${error.message}`)
+}
--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 const environment = process.env.NODE_ENV || 'development'
+// eslint-disable-next-line no-console
 console.log(environment)
 
 const config = require('../../knexfile')[environment]
