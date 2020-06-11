f1227a1353f59b35eb9672e6de8497f1e354bd3f
albasychris <albasychris@gmail.com>
2020-06-11 13:07:52 +1200

    write favouriteproducts routes

---
+++ server/routes/favouriteproducts.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const db = require('../db/favouriteproductsdb')
+const camelcaseKeys = require('camelcase-keys')
+const { isAdmin, isGetOwner } = require('../middleware/index')
+module.exports = router
+// GET /api/v1/favouriteproducts/:id
+router.get('/:id', (req, res) => {
+  db.getUserFavourites(req.params.id)
+    .then(camelcaseKeys)
+    .then(favs => res.status(200).json(favs))
+    .catch(err => res.status(500).send(err.message))
+})
+
+// POST /api/v1/favouriteproducts/
+router.post('/', (req, res) => {
+  db.addUserFavourite(req.body)
+    .then(() => res.status(500).send())
+    .catch(err => res.status(500).send(err.message))
+})
+
+// DELETE /api/v1/favouriteproducts/
+router.delete('/', (req, res) => {
+  db.deleteUserFavourite(req.body)
+    .then(() => res.status(500).send())
+    .catch(err => res.status(500).send(err.message))
+})
