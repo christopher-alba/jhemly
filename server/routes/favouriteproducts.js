0e2da53a8025901d95ee0a11794ed36438b85b2b
albasychris <albasychris@gmail.com>
2020-07-07 09:58:48 +1200

    apply snakecaseKeys to favouriteProducts routes

--- server/routes/favouriteproducts.js
+++ server/routes/favouriteproducts.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../db/favouriteproductsdb')
 const camelcaseKeys = require('camelcase-keys')
+const snakecaseKeys = require('snakecase-keys')
 const { isLoggedIn } = require('../middleware/index')
 module.exports = router
 // GET /api/v1/favouriteproducts/:id
@@ -13,14 +14,14 @@ router.get('/:id', (req, res) => {
 
 // POST /api/v1/favouriteproducts/
 router.post('/', (req, res, next) => isLoggedIn(next), (req, res) => {
-  db.addUserFavourite(req.body)
+  db.addUserFavourite(snakecaseKeys({ ...req.body }))
     .then(() => res.status(200).send())
     .catch(err => res.status(500).send(err.message))
 })
 
 // DELETE /api/v1/favouriteproducts/
 router.delete('/', (req, res, next) => isLoggedIn(next), (req, res) => {
-  db.deleteUserFavourite(req.body)
+  db.deleteUserFavourite(snakecaseKeys({ ...req.body }))
     .then(() => res.status(200).send())
     .catch(err => res.status(500).send(err.message))
 })
