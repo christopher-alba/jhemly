bf1b2ca4cb5da71e33ffc3fcba0ba20feb2d2f6d
albasychris <albasychris@gmail.com>
2020-07-07 10:06:42 +1200

    apply snakecaseKeys in reviews routes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,7 @@
 const reviewsdb = require('../db/reviewsdb')
 const express = require('express')
 const camelcaseKeys = require('camelcase-keys')
+const snakecaseKeys = require('snakecase-keys')
 const router = express.Router()
 const { isGetOwner, isLoggedIn, isFromOwner } = require('../middleware/index')
 
@@ -28,7 +29,7 @@ router.get('/:id', isGetOwner, (req, res) => {
 
 // POST /api/v1/reviews/:id
 router.post('/', (req, res, next) => isLoggedIn(next), (req, res) => {
-  reviewsdb.addReview(req.body)
+  reviewsdb.addReview(snakecaseKeys({ ...req.body }))
     .then(camelcaseKeys)
     .then(allreviews => {
       res.status(200).json(allreviews)
@@ -38,7 +39,7 @@ router.post('/', (req, res, next) => isLoggedIn(next), (req, res) => {
 
 // PUT /api/v1/reviews/:id
 router.put('/:id', (req, res, next) => isFromOwner('review', req, res, next), (req, res) => {
-  reviewsdb.updateReview(req.body, req.params.id)
+  reviewsdb.updateReview(snakecaseKeys({ ...req.body }), req.params.id)
     .then(camelcaseKeys)
     .then(updated => {
       res.status(200).json(updated)
